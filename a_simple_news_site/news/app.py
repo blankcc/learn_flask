fd6f6afbd94617692b8ddb8545d3dada82758189
894058546 <894058546@qq.com>
2018-02-16 22:11:32 +0800

    this is a completed edition

---
+++ a_simple_news_site/news/app.py
@@ -0,0 +1,45 @@
+import os
+import json
+from flask import Flask,render_template,abort
+
+app = Flask(__name__)
+
+class Files(object):
+	directory = '/home/shiyanlou/files'
+
+	def __init__(self):
+		self._files = self._get_files()
+
+	def _get_files(self):
+		result={}
+		for filename in os.listdir(self.directory):
+			file_path = os.path.join(self.directory,filename)
+			with open(file_path) as f:
+				result[filename[:-5]] = json.load(f)
+		return result
+    
+	def get_all_titles(self):
+		return [item['title'] for item in self._files.values()]
+
+	def get_file_by_filname(self,filename):
+		return self._files.get(filename)
+
+files = Files()
+
+@app.route('/')
+def index():
+	return render_template('index.html',titles = files.get_all_titles())
+
+@app.route('/files/<filename>')
+def file(filename):
+	file_item = files.get_file_by_filname(filename)
+	if not file_item:
+		abort(404)
+	return render_template('file.html',file_item = file_item)
+		
+@app.errorhandler(404)
+def not_found(error):
+	return render_template('404.html'),404
+
+if __name__ == '__main__':
+	app.run(port=3000)
