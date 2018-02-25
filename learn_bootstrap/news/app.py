d012a08c02c06306797f2b371ee57d0bebc0c6f6
894058546 <894058546@qq.com>
2018-02-25 16:57:09 +0800

    first edition

---
+++ learn_bootstrap/news/app.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,render_template,abort
+from pymongo import MongoClient
+
+app = Flask(__name__)
+app.config.update(dict(
+	SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/shiyanlou'
+))
+db = SQLAlchemy(app)
+mongo = MongoClient('127.0.0.1',27017).shiyanlou
+
+class File(db.Model):
+	__tablename__ = 'files'
+	id =db.Column(db.Integer,primary_key = True)
+	title = db.Column(db.String(80),unique = True)
+	create_time = db.Column(db.DateTime)
+	category_id = db.Column(db.Integer,db.ForeignKey('categories.id'))
+	category = db.relationship('Category',uselist = False)
+	content = db.Column(db.Text)
+
+	def __init__(self,title,create_time,category,content):
+		self.title = title
+		self.create_time = create_time
+		self.category = category
+		self.content = content
+
+	def add_tag(self,tag_name):
+		file_item = mongo.file.find_one({'file_id':self.id})
+		if file_item:
+			tags = file_item['tags']
+			if tag_name not in tags:
+				tags.append(tag_name)
+			mongo.file.update_one({'file_id':self.id},{'$set':{'tags':tags}})
+		else:
+			tags = [tag_name]
+			mongo.file.insert_one({'file_id':self.id,'tags':tags})
+		return tags
+	def remove_tag(self,tag_name):
+		file_item = mongo.file.find_one({'file_id':self.id})
+		if file_item:
+			tags = file_item['tags']
+			try:
+				tags.remove(tag_name)
+				new_tags = tags
+			except ValueError:
+				return tags
+			mongo.file.update_one({'file_id':self.id},{'$set':{'tags':new_tags}})
+			return new_tags
+		return []
+	@property
+	def tags(self):
+		file_item = mongo.file.find_one({'file_id':self.id})
+		if file_item:
+			print(file_item)
+			return file_item['tags']
+		else:
+			return[]
+
+class Category(db.Model):
+	__tablename__ = 'categories'
+	id = db.Column(db.Integer,primary_key = True)
+	name = db.Column(db.String(80),)
+	files = db.relationship('File')
+
+	def __init__(self,name):
+		self.name = name
+
+@app.route('/')
+def index():
+	return render_template('index.html',files = File.query.all())
+
+@app.route('/files/<file_id>')
+def file(file_id):
+	file_item = File.query.get_or_404(file_id)
+	return render_template('file.html',file_item = file_item)
+		
+@app.errorhandler(404)
+def not_found(error):
+	return render_template('404.html'),404
+
+if __name__ == '__main__':
+	app.run()
